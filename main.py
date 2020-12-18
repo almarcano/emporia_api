acb671504c833553c7abbccf3664ee7e5ab5225d
Alcides Marcano <almarcano@iMac-Alcides.local>
2020-12-17 20:56:01 -0500

    correcciones

--- main.py
+++ main.py
@@ -13,9 +13,6 @@ from pyemvue.enums import Scale, Unit, TotalTimeFrame, TotalUnit
 from pyemvue.device import VueDevice, VueDeviceChannel, VuewDeviceChannelUsage
 
 vue = PyEmVue()
-now = datetime.utcnow()
-startUse = datetime(2020, 11, 1, 5, 0, 0)
-endUse = datetime(2020, 12, 1, 6, 0, 0)
 
 def main():
     errorMsg = 'Please pass a file containing the "email" and "password" as json.'
@@ -57,24 +54,31 @@ def main():
         print('Not enough details to log in.', errorMsg)
         sys.exit(1)
     # LOGIN
-    vue.login(email, passw, idToken, accessToken, refreshToken, token_storage_file='keys.json')
+    vue.login(email, passw, idToken, accessToken, refreshToken, token_storage_file=filepath)
     # CUSTOMER DATA
     customer = vue.get_customer_details()                   # Get customer details
     print("Customer ID: " + str(customer.customer_gid))
     print("Nombre: " + customer.first_name)
     print("Apellidos: " + customer.lastName)
     print("Email: " + customer.email)
-    print("Creado: " + customer.created_at)
+    created_str = str(customer.created_at).replace("Z", "")                 # Remueve TIMEZONE de created_at
+    created_at = datetime.strptime(created_str, "%Y-%m-%dT%H:%M:%S.%f")     # Convierte created_str a datetime nuevamente
+    created_at = created_at.replace(minute=0, second=0, microsecond=0)
+    print(f"Creado: {created_at}")
     print()
     # DEVICES DETAILS
-    devices = vue.get_devices()                             # Get devices details
+    devices = vue.get_devices()
+    now = datetime.utcnow()
+    start_date = created_at                     # Para asignar fecha directamente datetime(2020, 11, 1, 5, 0, 0)
+    end_date = now                              # Get devices details
     for i, device in enumerate(devices):
         device = vue.populate_device_properties(device)
-        totalUsage = vue.get_usage_over_time(devices[i].channels[0], startUse, endUse, scale=Scale.HOUR.value, unit=Unit.WATTS)
+        device_name = "Dispositivo " + str(i) if device.device_name == None else device.device_name
+        total_usage = vue.get_usage_over_time(devices[i].channels[0], start_date, end_date, scale=Scale.HOUR.value, unit=Unit.WATTS)
         # Crear Archivos CSV
-        #create_csv(device.device_name, totalUsage)          
-        create_plot(device.device_name, startUse, endUse)   # Crea Gráficos en html
-        print(device.device_name)                           # Nombre del equipo
+        create_csv(device_name, total_usage, start_date)          
+        create_plot(device_name, start_date, end_date)      # Crea Gráficos en html
+        print("Dispositivo: " + device_name)                # Nombre del equipo
         print("GID: " + str(device.device_gid))             # GID del equipo
         print("Modelo: " + device.model)                    # Modelo del equipo
         print("Device ID: " + '-'.join(device.manufacturer_id.upper()[i:i+4] for i in range(0,16,4)))  # Serial del equipo
@@ -86,15 +90,15 @@ def main():
         print(round(vue.get_total_usage(devices[i].channels[0], TotalTimeFrame.ALL.value) / 1000, 2), "kwh used TOTAL")
         print()  
 
-def create_csv(name, total):
+def create_csv(name, total, start_date):
     filename = name + ".csv"
-    fieldNames = ["DateTime", "Value"]
-    startTime = startUse + timedelta(hours=-5)
+    field_names = ["DateTime", "Value"]
+    start_time = start_date + timedelta(hours=-4)
     with open(filename, "w", newline="") as f:
-        writer = csv.DictWriter(f, fieldnames=fieldNames)
+        writer = csv.DictWriter(f, fieldnames=field_names)
         writer.writeheader()
         for i, line in enumerate(total):
-            date_and_time = startTime + timedelta(hours=+i)
+            date_and_time = start_time + timedelta(hours=+i)
             if line is None:
                 kwh = 0.0   
             else: 
@@ -102,7 +106,7 @@ def create_csv(name, total):
             writer.writerow({"DateTime":date_and_time, "Value":kwh})
             # Procedimiento adicional para corregir hora (2020,11,1,1,0,0) REPETIDA
             if date_and_time == datetime(2020, 11, 1, 1, 0, 0):
-                startTime = startUse + timedelta(hours=-6)
+                start_time = start_date + timedelta(hours=-5)
 
 def create_plot(name, start, end):
     csv_file = name + ".csv"
